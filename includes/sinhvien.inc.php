4fef2c6baf19e0e3804da038d8dd02eb271fef61
Y Pham <pxy.uit@gmail.com>
2020-03-06 23:08:32 +0700

    [Update] - Xu ly form them sinh vien

---
+++ includes/sinhvien.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+include '../autoload/autoload.php';
+
+$msv = $_POST['msv'];
+$ho = $_POST['ho'];
+$ten = $_POST['ten'];
+$gioi_tinh = $_POST['gioi_tinh'];
+$ngay_sinh = $_POST['ngay_sinh'];
+$cmnd = $_POST['cmnd'];
+$sdt = $_POST['sdt'];
+$dia_chi = $_POST['dia_chi'];
+
+$loai_du_lieu = $_POST['loai_du_lieu'];
+
+switch ($loai_du_lieu) {
+  case 'sinh_vien':
+    $sinh_vien = new SinhVien($cmnd, $ho, $ten, $ngay_sinh, $gioi_tinh, $dia_chi, $sdt, $msv);
+    echo $sinh_vien->getMaSinhVien();
+    break;
+
+    default:
+    echo "Error!";
+    break;
+}
+
+?>
