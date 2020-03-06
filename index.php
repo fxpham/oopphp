1c0c775f35d6669a396a6e6e2149850ddf741dc1
Y Pham <pxy.uit@gmail.com>
2020-03-06 23:07:25 +0700

    [Update] - Giao dien Tong quan

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 
 <!doctype html>
-<html lang="vi">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Bai tap</title>
-    <!-- Bootstrap core CSS -->
+    <title>Dashboard Template · Bootstrap</title>
+
     <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <meta name="theme-color" content="#563d7c">
-
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -25,99 +24,36 @@
         }
       }
     </style>
+    <!-- Custom styles for this template -->
+    <link href="./assets/css/dashboard.css" rel="stylesheet">
   </head>
 
-  <body class="bg-light">
-    <div class="container">
-  <div class="py-5 text-center">
-    <h2>Nhập thông tin sinh viên</h2>
-    <p class="lead">Điền các field bên dưới để thêm sinh viên.</p>
-  </div>
+  <body>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Quản lý sinh viên</a>
+    </nav>
 
-  <div class="row">
-    <div class="col-md-12 order-md-1">
-      <h4 class="mb-3">Thông tin</h4>
-      <form class="needs-validation" novalidate>
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="./">Tổng quan</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./sinh_vien.php">Sinh viên</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-        <div class="mb-3">
-          <label for="msv">Mã sinh viên</label>
-          <div class="input-group">
-            <input type="text" class="form-control" id="msv" placeholder="Mã sinh viên" required>
-            <div class="invalid-feedback" style="width: 100%;">
-              Mã sinh viên là bắt buộc.
-            </div>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">Tổng quan</h1>
           </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="lastName">Họ</label>
-            <input type="text" class="form-control" id="lastName" placeholder="Nguyễn Văn" value="" required>
-            <div class="invalid-feedback">
-              Valid last name is required.
-            </div>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="firstName">Tên</label>
-            <input type="text" class="form-control" id="firstName" placeholder="A" value="" required>
-            <div class="invalid-feedback">
-              Valid first name is required.
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="gioitinh">Giới tính</label>
-            <select class="custom-select d-block w-100" id="gioitinh" required>
-              <option value="">Chọn...</option>
-              <option value="nam">Nam</option>
-              <option value="nu">Nữ</option>
-            </select>
-            <div class="invalid-feedback">
-              Vui lòng chọn giới tính.
-            </div>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="dob">Ngày sinh</label>
-            <input type="date" class="form-control" id="dob" required>
-            <div class="invalid-feedback">
-              Vui lòng nhập Ngày sinh.
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-6 mb-3">
-            <label for="cmnd">Chứng minh nhân dân</label>
-            <input type="text" class="form-control" id="cmnd" placeholder="1122334455" required>
-            <div class="invalid-feedback">
-              Vui lòng nhập chứng minh nhân dân.
-            </div>
-          </div>
-          <div class="col-md-6 mb-3">
-            <label for="sdt">Số điện thoại</label>
-            <input type="text" class="form-control" id="sdt" placeholder="0123456789" required>
-            <div class="invalid-feedback">
-              Vui lòng nhập số điện thoại.
-            </div>
-          </div>
-        </div>
-
-        <div class="mb-3">
-          <label for="address">Địa chỉ</label>
-          <input type="text" class="form-control" id="address" placeholder="123 Trần Hưng Đạo, Quận 1, TP.Hồ Chí Minh">
-          <div class="invalid-feedback">
-            Vui lòng nhập thông tin địa chỉ.
-          </div>
-        </div>
-
-        <hr class="mb-4">
-        <button class="btn btn-primary btn-lg btn-block" type="submit">Lưu</button>
-      </form>
+        </main>
+      </div>
     </div>
-  </div>
-
-</div>
-
+  </body>
 </html>
