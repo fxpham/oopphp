35361cd5ce2786eccaca27d566062772685860d4
Nguyen Hong <nguyenhonguef@gmail.com>
2020-03-10 15:59:47 +0700

    [update] ket noi database don gian

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
+//Kết nối database
+mysql_connect('localhost','root','');
+//lựa chọn database
+mysql_select_db('c5_bt1_user');
+
+?>
+
+<?php
+$sql=mysql_query('SELECT * FROM dangnhap');
+while($value=mysql_fetch_array($sql))
+{
+	echo $value['username'].'<br/>';
+	echo $value['email'].'<br/>';
+}
 ?>
