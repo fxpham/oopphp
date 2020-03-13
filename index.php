dc24912f7c162723983b0692d522464221c02770
Nguyen Hong <nguyenhonguef@gmail.com>
2020-03-13 22:14:12 +0700

    [update]_chinh sua loi ket noi

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDBPDO";
+$servername = "localhost:3306";
+$username = "nguyenhong";
+$password = "123456";
+$dbname = "mydb";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
-    VALUES ('John', 'Doe', 'john@example.com')";
+    $sql = "INSERT INTO user (ten, ho, email)
+    VALUES ('John1', 'Doe1', 'john1@example.com')";
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "New record created successfully";
