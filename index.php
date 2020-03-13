ad0387c872c51f1b1595ea33d350b8d872b10c90
Nguyen Hong <nguyenhonguef@gmail.com>
2020-03-13 15:31:12 +0700

    [update]_Create table

--- index.php
+++ index.php
@@ -2,15 +2,25 @@
 $servername = "localhost";
 $username = "username";
 $password = "password";
+$dbname = "myDBPDO";
 
 try {
-    $conn = new PDO("mysql:host=$servername", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE myDBPDO";
+
+    // sql to create table
+    $sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+
     // use exec() because no results are returned
     $conn->exec($sql);
-    echo "Database created successfully<br>";
+    echo "Table MyGuests created successfully";
     }
 catch(PDOException $e)
     {
