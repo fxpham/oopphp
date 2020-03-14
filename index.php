f21c9ef0ed8882b9fca76e7bad2442264a7d2806
Y Pham <pxy.uit@gmail.com>
2020-03-14 15:57:01 +0700

    [BaitapDB] - Connect Database

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
+//khai bao thong tin ket noi
+$servername = "localhost:3306"; //localhost: ten miền server; 3306: la port mysql, xem tren xamp
+$username = "nguyenhong"; //thông tin tài khoản của mysql (mỗi ng phải tự tạo), ko được trùng
+$password = "123456"; //thông tin tài khoản
+$dbname = "mydb"; // tên csdl để kết nối đến
+
+try {
+    //tạo 1 đối tượng thuộc class PDO
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+    // set the PDO error mode to exception
+    //thiết lập các Attribute của PDO, nếu kết nối được thì echo Thành Công
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Kết nối thành công";
+} catch(PDOException $e) {
+    //nếu kết nối thất bại thì in ra 1 thông báo lỗi
+    echo "Kết nối thất bại " . $e->getMessage();
+}
 ?>
+
