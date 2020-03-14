e8c78ff47d6eb10295af7659d2960927cb4addf5
Nguyen Hong <nguyenhonguef@gmail.com>
2020-03-14 15:11:49 +0700

    [update]_đổi tên databse, thêm record mới

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 $servername = "localhost:3306";
 $username = "nguyenhong";
 $password = "123456";
-$dbname = "mydb";
+$dbname = "db1";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO user (ten, ho, email)
-    VALUES ('John1', 'Doe1', 'john1@example.com')";
+    $sql = "INSERT INTO user (firstname, lastname, email)
+    VALUES ('Hong', 'Nguyen', 'john@example.com')";
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "New record created successfully";
