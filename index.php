f1330d88ac00cb265706fc4b78e29a21d00cf28b
Nguyen Hong <nguyenhonguef@gmail.com>
2020-03-14 15:41:52 +0700

    [update]_giải thích code

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
 <body>
 
 <?php
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+//Tạo 1 bảng table 
 
+echo "<table style='border: solid 1px black;'>";
+//render header của bảng
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th><th>Email</th><th>Regiter date</th></tr>";
+
+//Render ra 1 table
 class TableRows extends RecursiveIteratorIterator {
     function __construct($it) {
         parent::__construct($it, self::LEAVES_ONLY);
@@ -28,17 +32,22 @@ class TableRows extends RecursiveIteratorIterator {
 $servername = "localhost:3306";
 $username = "nguyenhong";
 $password = "123456";
-$dbname = "mydb";
+$dbname = "db1";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM user");
-    $stmt->execute();
+    $sql="SELECT * FROM user";//chọn all dữ liệu từ bảng user
+    $stmt = $conn->prepare($sql);//PDO chuẩn bị 1 câu truy vấn, 
+    $stmt->execute(); //thực thi câu lệnh sql
 
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    // Kết quả trả về dạng mảng lồng nhau (Fetch_ASSOC)
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    // $data = $stmt->fetchAll();
+    // print_r($data);//
+    // exit;
 
+    //Render dữ liệu ra HTML 
     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
         echo $v;
     }
