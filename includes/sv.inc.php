c6943730e6d1be6894db63998452aa33037afcfd
Nguyen Hong <nguyenhonguef@gmail.com>
2020-03-20 15:33:53 +0700

    [update]_lam bai tap 10

---
+++ includes/sv.inc.php
@@ -0,0 +1,23 @@
+<?php
+  $masv = $_POST['masv'];
+  $tensv = $_POST['tensv'];
+  
+  $servername = "localhost:3306";
+  $username = "nguyenhong";
+  $password = "123456";
+  $dbname = "bt9";
+
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "UPDATE sinh_vien (MaSV,ten)
+    VALUES ('$masv','$tensv')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Da sửa sinh vien trong bảng bt9 ";
+  }
+  catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+?>
--- index.php
+++ index.php
@@ -1,2 +1,38 @@
-<?php
-?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div class="1">
+  <h1 class="h2">THÔNG TIN CẬP NHẬT</h1>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 order-md-1">
+      <form action="./includes/sv.inc.php" method="post" class="needs-validation" novalidate="novalidate">
+        <div class="mb-3">
+          <table width="366" border="1">
+            <tr>
+              <td width="142">Mã sinh viên :</td>
+              <td width="208"><label for="Idsv"></label>
+              <input type="text" name="masv" id="masv" /></td>
+            </tr> 
+            <tr>
+              <td>Tên sinh viên mới: </td>
+              <td><label for="tensv"></label>
+              <input type="text" name="tensv" id="tensv" /></td>
+            </tr>
+          </table>
+        </div>
+        <hr class="mb-4" />
+        <button class="2" type="submit" >Cập nhật</button>
+        <button class="2" type="submit" >Hủy</button>
+        </form>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
