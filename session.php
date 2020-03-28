ae230209fcea2e3c0da22679889ecf0e06c9698b
Y Pham <pxy.uit@gmail.com>
2020-03-28 22:43:49 +0700

    [Session] - Session example

---
+++ session.php
@@ -0,0 +1,24 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <body>
+
+    <?php
+    // Show all session variable values
+    // print_r($_SESSION);
+
+    // Echo session variables that were set on previous page
+    echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+    echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+
+    // remove all session variables
+    session_unset();
+
+    // destroy the session
+    session_destroy();
+    ?>
+
+  </body>
+</html>
