eef93d80ee7734a666908bcd5c6767a6cbc644bd
Y Pham <pxy.uit@gmail.com>
2020-03-28 22:56:33 +0700

    [Cookie] - Cookie example

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
+  // Create | Retrieve a cookie.
+  $cookie_name = "user";
+  $cookie_value = "John Doe";
+  setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+  // Modify cookie value by set again.
+  // To delete cookie use setcookie() with an expiration date in the past.
+  // setcookie("user", "", time() - 3600);
 ?>
+<html>
+  <body>
+
+  <?php
+    if(!isset($_COOKIE[$cookie_name])) {
+      echo "Cookie named '" . $cookie_name . "' is not set!";
+    } else {
+      echo "Cookie '" . $cookie_name . "' is set!<br>";
+      echo "Value is: " . $_COOKIE[$cookie_name];
+    }
+  ?>
+
+  </body>
+</html>
