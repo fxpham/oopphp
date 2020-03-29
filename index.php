dcb0f42c30f95fc43b70e6751dcd619c65bad047
Y Pham <pxy.uit@gmail.com>
2020-03-29 09:06:52 +0700

    [Update] - Xu ly file

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-  $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
-  echo fread($myfile,filesize("webdictionary.txt"));
+// Mở file ở chế độ đọc.
+  $myfile = fopen("./file/newfile.txt", "r") or die("Unable to open file!");
+  // Đọc toàn bộ nội dung file
+  // echo fread($myfile,filesize("./file/newfile.txt"));
+  // Đọc từng dòng
+  // feof: Kiểm tra đã cuối file chưa.
+  while(!feof($myfile)) {
+    // Đọc từng dòng trong file.
+    echo fgets($myfile) . "<br>";
+  }
+  //Đóng file.
   fclose($myfile);
 ?>
--- write.php
+++ write.php
@@ -1,8 +1,11 @@
 <?php
+  // Mở file mới ở chế độ ghi.
   $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-  $txt = "John Doe\n";
+  $txt = "Nguyen Hong\n";
+  // Ghi nội dung trong biến txt xuống file (newfile.txt)
   fwrite($myfile, $txt);
-  $txt = "Jane Doe\n";
+  $txt = "Nhat Quang\n";
   fwrite($myfile, $txt);
+  // Đóng file.
   fclose($myfile);
 ?>
